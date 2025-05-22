e5d648575e0a10df06077ed68f7083de05baaa82
Kespion <kesputecryo@gmail.com>
2025-05-22 13:57:03 +0200

    hide button return

--- lib/pages/navigation_pages/home_page.dart
+++ lib/pages/navigation_pages/home_page.dart
@@ -240,7 +240,7 @@ class _HomePageState extends State<HomePage> {
               ],
 
               const SizedBox(height: 15),
-
+/*
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -265,7 +265,7 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ],
               ),
-
+*/
             ],
           ),
         ),
